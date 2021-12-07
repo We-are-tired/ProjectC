5587a1e2f02afe426da5228afd2c6fb09409dc3a
ushiwaka7520 <21cm0117@jec.ac.jp>
2021-12-07 16:01:23 +0900

    12/7_設定Viewを変更

--- OurTeamSeisaku/GameSetting.swift
+++ OurTeamSeisaku/GameSetting.swift
@@ -1,126 +1,225 @@
 //
 //  GameSetting.swift
-//  BarrageGame
+//  OurTeamSeisaku
 //
-//  Created by cmStudent on 2021/11/29.
+//  Created by cmStudent on 2021/12/06.
 //
 
 import SwiftUI
 
 struct GameSetting: View {
-    
-//    @State private var iphoneBtn = true
-//    @State private var gameSpeed = true
-//    @State private var highCastTime = true
-//    @State private var appleCare = true
-    
-//    @State private var viewModel.isiPhoneSize = true
-//    @State private var viewModel.isSpeed = true
-//    @State private var viewModel.isHighCastTime = true
-//    @State private var viewModel.isAppleCare = true
-    
     @ObservedObject var viewModel = GameSettingViewModel()
     var kidsNum = (1,5)
+    
     var body: some View {
-        
-        //HStack{時間設定を横に出す時に必要になる
-            VStack{//引数エラーが起こるため、VStack多め。
+        ZStack{
+            Image("Setting_pic")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            HStack{//ユーザーの横並び
+                ///左側のユーザー（初期値は落とす側）
+                VStack{
+                    VStack{
+                        Button{
+                            //タップでユーザー画像の変更を行う想定
+                        }label:{
+                            //以下のアイコンは画像に変更予定。仮置きのSFシンボル。
+                            Image("kids\(kidsNum.0)")
+                                .resizable()
+                                .frame(width: 55, height: 55)
+                                .padding()
+                                .background(Color.white)
+                        }//ユーザー1のボタン
+                        
+                        TextField("ユーザー1", text: $viewModel.userName1)
+                            .padding()
+                            .background(Color.white)
+                        
+                        Spacer().frame(height:50)
+                        
+                    }//VStack(ユーザー1全般)
+                    
+                    VStack{
+                        Text("障害物")
+                            .padding(2.0)
+                            .background(Color.white)
+                        
+                        HStack(spacing:15){
+                            Button{
+                                //enemyItemsの画像切り替え（左）
+                            }label:{
+                                Text("←")
+                                    .padding(5)
+                                    .background(Color.white)
+                            }//障害物・左のボタン
+                            
+                            //障害物のアイコン
+                            Image(systemName:"\(viewModel.enemyItems)")
+                                .resizable()
+                                .frame(width: 40, height: 35)
+                                .padding()
+                                .background(Color.white)
+                            
+                            Button{
+                                //enemyItemsの画像切り替え（右）
+                            }label:{
+                                Text("→")
+                                    .padding(5)
+                                    .background(Color.white)
+                            }//障害物・右のボタン
+                        }//HStack(障害物ボタン全般)
+                        
+                        Spacer().frame(height:50)
+                        
+                    }//VStack(障害物)
+                    
+                    VStack{
+                        Text("速度")
+                            .padding(2.0)
+                            .background(Color.white)
+                        
+                        HStack(spacing:10){
+                            Button{
+                                viewModel.itemsSpeed -= 1
+                                
+                                if(viewModel.itemsSpeed < 1){
+                                    //速度を1以下にしない処理
+                                    viewModel.itemsSpeed = 1
+                                }
+                            }label:{
+                                Text("←")
+                                    .padding(5)
+                                    .background(Color.white)
+                            }//速度・左のボタン
+                            
+                            Text("\(viewModel.itemsSpeed)")
+                                .padding()
+                                .background(Color.white)
+                            
+                            Button{
+                                viewModel.itemsSpeed += 1
+                                
+                                if(viewModel.itemsSpeed > 5){
+                                    viewModel.itemsSpeed = 5
+                                }
+                            }label:{
+                                Text("→")
+                                    .padding(5)
+                                    .background(Color.white)
+                            }//速度・右のボタン
+                        }//速度ボタン全般
+                        
+                        Spacer().frame(height:80)
+                        
+                    }//VStack(Speed)
+                }//VStack(User_All:左側)
+                
+                //ユーザー同士のスペース
+                Spacer().frame(width:60)
+                
+                ///右側のユーザー（初期値は避ける側）
+                VStack{
+                    VStack{
+                        Button{
+                            //タップでユーザー画像の変更を行う想定
+                        }label:{
+                            //以下のアイコンは画像に変更予定。仮置きのSFシンボル。
+                            Image("kids\(kidsNum.0)")
+                                .resizable()
+                                .frame(width: 55, height: 55)
+                                .padding()
+                                .background(Color.white)
+                        }//ユーザー2の画像
+                        
+                        TextField("ユーザー2", text: $viewModel.userName2)
+                            .padding()
+                            .background(Color.white)
+                        
+                        Spacer().frame(height:50)
+                    }//VStack（ユーザー2全般）
+                    
+                    VStack{
+                        Text("\(viewModel.iphoneName)")
+                            .padding(2.0)
+                            .background(Color.white)
+                        
+                        HStack(spacing:15){
+                            
+                            Button{
+                                //端末の大きさを変える処理
+                            }label:{
+                                Text("←")
+                                    .padding(5)
+                                    .background(Color.white)
+                                
+                            }
+                            
+                            Image(systemName:"\(viewModel.iphoneItems)")
+                                .resizable()
+                                .frame(width: 40, height: 35)
+                                .padding()
+                                .background(Color.white)
+                            
+                            Button{
+                                //端末の大きさを変える処理
+                            }label:{
+                                Text("→")
+                                    .padding(5)
+                                    .background(Color.white)
+                            }
+                        }
+                        
+                        Spacer().frame(height:65)
+                        
+                    }//VStack(Size)
+                    
+                    VStack(spacing:10){
+                        Text("Apple Care")
+                            .padding(2.0)
+                            .background(Color.white)
+                        
+                        Text(viewModel.isAppleCare ? "" : "")
+                        Toggle("", isOn: $viewModel.isAppleCare)
+                            .labelsHidden()
+                        
+                        Spacer().frame(height:50)
+                        
+                    }//VStack(Apple Care)
+                }//VStack(User_All:右側)
+                
+            }//HStack（2人のユーザー横並び）
+            .padding()
+            
+            VStack{
+                
+                //入替ボタン(画面一番上)
+                Button{
+                    //ユーザー同士を入れ替える処理
+                }
+            label:{
+                Image(systemName: "repeat")
+                    .resizable()
+                    .frame(width: 55, height: 55)
+                    .padding()
+                    .background(Color.white)
+            }
                 
                 Spacer()
                 
-                VStack{
-                    Image("kids\(kidsNum.0)")
-                        .resizable()
-                        .frame(width: 55, height: 55)
-                    
-                    Text("Iphone ユーザー")
-                    
-                }//User
-                
-                VStack{
-                    Text("端末(小)")
-                        .padding()
-                    
-                    //Sample
-                    //Text(iphoneBtn2 ? "ON" : "OFF")
-                    //Toggle("", isOn: $iphoneBtn2)
-                    //    .labelsHidden()
-                    
-                    Text(viewModel.isiPhoneSize ? "" : "")
-                    Toggle("", isOn: $viewModel.isiPhoneSize)
-                        .labelsHidden()
-                }//Size
-                
-                VStack{
-                    Text("　高速　")
-                        .padding()
-                    
-                    Text(viewModel.isSpeed ? "" : "")
-                    Toggle("", isOn: $viewModel.isSpeed)
-                        .labelsHidden()
-                }//Speed
-                
-                VStack{
-                    Text(" CT減少 ")
-                        .padding()
-                    
-                    Text(viewModel.isHighCastTime ? "" : "")
-                    Toggle("", isOn: $viewModel.isHighCastTime)
-                        .labelsHidden()
-                }//CT
-                
-                
-                VStack{
-                    Text("Apple Care")
-                        .padding()
-                    
-                    Text(viewModel.isAppleCare ? "" : "")
-                    Toggle("", isOn: $viewModel.isAppleCare)
-                        .labelsHidden()
-                }//Apple Care
-                
-                
-                Spacer()
-                
+                //スタートボタン（画面一番下）
                 Button {
                     viewModel.settings()
                     mainView().changeView(view: "Newton")
                 } label: {
-                    Text("スタート")
+                    Text("Let's Play !")
                         .font(.title)
                         .padding()
+                        .background(Color.white)
+                        .border(Color.black)
                 }
-
-            }//VStack(All)
-            
-            /*時間設定用のスクリーン------------------------*/
-            //            VStack{
-            //
-            //            Text("時間")
-            //                .font(.title)
-            //                .padding()
-            //
-            //            HStack{
-            //                Text("30")
-            //
-            //                Text("60")
-            //
-            //                Text("無限")
-            //            }//HStack(30, 60, FreeTime)
-            //
-            //                Text("先攻")
-            //                    .padding()
-            //
-            //                HStack{
-            //                    Text("プレイヤー1")
-            //                    Text("プレイヤー2")
-            //                }
-            //
-            //                Spacer()
-            //            }VStack(TimeSetting Screen)
-            /*-------------------------------------------*/
-            
-        //}//HStack(All + Time)
+            }//Button(スタート：一番下に設置)
+        }//ZStack(背景)
     }//body
 }//GameSetting
 
--- OurTeamSeisaku/Settings/GameSettingViewModel.swift
+++ OurTeamSeisaku/Settings/GameSettingViewModel.swift
@@ -14,6 +14,16 @@ class GameSettingViewModel: ObservableObject {
     @Published var isAppleCare = true
     @Published var isiPhoneSize = true
     @Published var isHighCastTime = true
+    
+    //  設定画面(TwoUserSetting.swift)における変更に伴い、以下を追加
+        @Published var enemyItems = "flame.fill" //障害物（flame.fill、cloud.rain.fill、battery.100.boltで切り替え）
+        @Published var iphoneName = "端末(小)" //端末名
+        @Published var iphoneItems = "iphone.homebutton" //端末のアイコン（iphoneと切り替え）
+        @Published var userIcon1 = "person" //ユーザー1のアイコン
+        @Published var userIcon2 = "person.fill" //ユーザー2のアイコン
+        @Published var userName1 = "" //ユーザー1の名前
+        @Published var userName2 = "" //ユーザー2のアイコン
+        @Published var itemsSpeed = 1 //落下速度のレベル
 
     let model = Setting.model
     
